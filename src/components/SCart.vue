99f15ef076bf177ccfadb91a477072ccde255cbb
Kashirskiy Nikita <lordwebkit@gmail.com>
2022-12-10 20:51:49 +0300

    added title for empty cart, style for cart

--- src/components/SCart.vue
+++ src/components/SCart.vue
@@ -1,22 +1,30 @@
 <template>
-  <div class="s-cart">
-    <router-link :to="{ name: 'catalog' }">
-      <div class="s-catalog__link-cart">Back to Catalog</div>
-    </router-link>
-    <h2>Cart</h2>
-    <p v-if="CART.length === 0">There are no product in card</p>
-    <s-cart-item
-      v-for="(item, index) in CART"
-      :key="item.article"
-      :cart-item-data="item"
-      @delete-from-cart="deleteFromCart(index)"
-      @decrement-quantity-cart-item="decrementQuantityCartItem(index)"
-      @increment-quantity-cart-item="incrementQuantityCartItem(index)"
-    />
-    <div class="s-cart__total">
-      <p>Total:</p>
-      <p>{{ CART.length === 0 ? 0 : cartTotalCost }}</p>
-    </div>
+  <div class="s-cart-wrapper">
+    <section class="s-grid-wrapper">
+      <div class="s-cart__title-wrapper">
+        <h2 class="s-title s-cart__title">
+          Cart
+          <span :class="cartEmpty ? 's-cart_inline-block' : 's-cart_none'"
+            >is empty please add any thing
+          </span>
+        </h2>
+      </div>
+      <s-cart-item
+        v-for="(item, index) in CART"
+        :key="item.article"
+        :cart-item-data="item"
+        @delete-from-cart="deleteFromCart(index)"
+        @decrement-quantity-cart-item="decrementQuantityCartItem(index)"
+        @increment-quantity-cart-item="incrementQuantityCartItem(index)"
+      />
+      <div
+        class="s-cart__buy-wrapper"
+        :class="cartEmpty ? 's-cart_none' : 's-cart_flex'"
+      >
+        <p class="s-text s-cart__total">Total: {{ cartTotalCost }}$</p>
+        <button class="s-btn s-cart__buy">BUY</button>
+      </div>
+    </section>
   </div>
 </template>
 <script>
@@ -34,6 +42,9 @@ export default {
       return this.CART.reduce((sum, el) => {
         return sum + el.price * el.quantity;
       }, 0);
+    },
+    cartEmpty() {
+      return this.CART.length === 0 ? true : false;
     },
   },
   methods: {
@@ -54,4 +65,30 @@ export default {
   },
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.s-cart {
+  &_inline-block {
+    display: inline-block;
+  }
+  &__title {
+    &-wrapper {
+      margin: 20px 0 25px 0;
+    }
+  }
+  &__buy {
+    padding: 10px 30px;
+    &-wrapper {
+      margin-top: 20px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+  }
+  &_flex {
+    display: flex;
+  }
+  &_none {
+    display: none;
+  }
+}
+</style>
--- src/components/SCartItem.vue
+++ src/components/SCartItem.vue
@@ -5,18 +5,19 @@
       :src="getImageUrl(cartItemData.image)"
       alt="Photo T-shirt"
     />
-    <div class="s-cart-item__info">
+    <div class="s-cart-item__info s-text_s">
       <p>{{ cartItemData.name }}</p>
-      <p>{{ cartItemData.price }}</p>
-      <p>{{ cartItemData.article }}</p>
+      <p>{{ cartItemData.price }}$</p>
     </div>
-    <div class="s-cart-item__quantity">
+    <div class="s-cart-item__quantity s-text_xs">
       <p>Quantity</p>
-      <span @click="callDecrementQuantityCartItem">-</span>
-      {{ cartItemData.quantity }}
-      <span @click="callIncrementQuantityCartItem">+</span>
+      <div class="s-cart-item-quantity__info">
+        <span class="s-cart-item-quantity__info-minus" @click="callDecrementQuantityCartItem"></span>
+        {{ cartItemData.quantity }}
+        <span class="s-cart-item-quantity__info-plus" @click="callIncrementQuantityCartItem"></span>
+      </div>
     </div>
-    <button @click="callDeleteFromCart">Delete</button>
+    <button class="s-btn s-cart-item__btn " @click="callDeleteFromCart">Delete</button>
   </div>
 </template>
 <script>
@@ -57,15 +58,38 @@ export default {
 </script>
 <style lang="scss" scoped>
 .s-cart-item {
+  margin-top: 12px;
   padding: $padding * 2;
   display: flex;
   justify-content: space-between;
   box-shadow: 0 0 8px 0 #e0e0e0;
   align-items: center;
   flex-wrap: nowrap;
-
   &__image {
     max-width: 80px;
+  }
+  &__btn {
+    padding: 5px 10px;
+  }
+  &-quantity {
+    &__info {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      &-minus,
+      &-plus {
+        width: 15px;
+        height: 15px;
+        background-size: 15px;
+        cursor: pointer;
+      }
+      &-minus {
+        background-image: url('@/assets/icons/minus.svg');
+      }
+      &-plus {
+        background-image: url('@/assets/icons/plus.svg');
+      }
+    }
   }
 }
 </style>
