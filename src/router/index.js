6d32d87521af9859ac8de60158fa6f3e49ea1e3d
Kashirskiy Nikita <lordwebkit@gmail.com>
2022-12-13 10:33:57 +0300

    Added new path for main component

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
+import SMain from "@/components/SMain.vue";
 import SCatalog from "@/components/SCatalog.vue";
 import SCart from "@/components/SCart.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: "/", name: "catalog", component: SCatalog },
+    { path: "/", name: "main", component: SMain },
+    { path: "/catalog", name: "catalog", component: SCatalog },
     { path: "/cart", name: "cart", component: SCart, props: true },
   ],
 });
