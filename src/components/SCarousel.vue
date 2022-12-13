1b803704b3a4f034db7be63b72f2a8d5a9b089ce
Kashirskiy Nikita <lordwebkit@gmail.com>
2022-12-13 10:34:45 +0300

    Created Infinity slider

---
+++ src/components/SCarousel.vue
@@ -0,0 +1,152 @@
+<template>
+  <div class="s-carousel-wrapper">
+    <div class="s-carousel">
+      <s-carousel-item
+        v-for="item in carouselData"
+        :key="item"
+        :carousel-item-data="item"
+        :is-animated="isAnimated"
+      />
+    </div>
+    <div class="s-carousel-nav">
+      <button
+        class="s-carousel-nav__button s-carousel-nav__prev"
+        @click="prevSlide"
+      ></button>
+      <button
+        class="s-carousel-nav__button s-carousel-nav__next"
+        @click="nextSlide"
+      ></button>
+    </div>
+  </div>
+</template>
+<script>
+import SCarouselItem from "./SCarouselItem.vue";
+
+export default {
+  name: "SCarousel",
+  components: {
+    SCarouselItem,
+  },
+  props: {
+    carouselData: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+    interval: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data() {
+    return {
+      carouselDataExtended: [],
+      isAnimated: true,
+      currentSlideIndex: 1,
+    };
+  },
+  methods: {
+    prevSlide() {
+      this.carouselData.forEach((ext) => {
+        ext.translate++;
+      });
+      this.currentSlideIndex--
+      if (this.currentSlideIndex === 0) this.prevMagic()
+    },
+    nextSlide() {
+      this.carouselData.forEach((ext) => {
+        ext.translate--;
+      });
+      this.currentSlideIndex++
+      if (this.currentSlideIndex === 4) this.nextMagic()
+    },
+    prevMagic() {
+      const vm = this
+      setTimeout(() => {
+        vm.isAnimated = false;
+        vm.carouselData.forEach((ext) => {
+          ext.translate = ext.translate - 3;
+        })
+        vm.currentSlideIndex = vm.currentSlideIndex + 3
+      }, 1000)
+      setTimeout(() => {
+        this.isAnimated = true
+      }, 1020)
+    },
+    nextMagic() {
+      const vm = this
+      setTimeout(() => {
+        vm.isAnimated = false;
+        vm.carouselData.forEach((ext) => {
+          ext.translate = ext.translate + 3;
+        })
+        vm.currentSlideIndex = vm.currentSlideIndex - 3
+      }, 1000)
+      setTimeout(() => {
+        this.isAnimated = true
+      }, 1020)
+    },
+  },
+};
+</script>
+<style lang="scss">
+.s-carousel {
+  display: flex;
+  width: 6000px;
+  height: 100%;
+  &-wrapper {
+    width: 1050px;
+    height: 100%;
+    overflow: hidden;
+
+    &:hover {
+      .s-carousel-nav__button {
+        &::after {
+          opacity: 1;
+        }
+      }
+    }
+  }
+  &-nav {
+    transform: translate(0, -100%);
+    display: flex;
+    justify-content: space-between;
+
+    &__button {
+      width: 300px;
+      display: flex;
+      align-items: center;
+      height: 305px;
+      padding: 20px;
+      border: 0;
+      background-color: inherit;
+
+      &:hover {
+        cursor: pointer;
+      }
+
+      &::after {
+        content: "";
+        transition: all 2s ease;
+        display: block;
+        opacity: 0;
+        width: 70px;
+        height: 39px;
+        background-image: url("@/assets/icons/arrow.svg");
+        background-size: 100%;
+      }
+    }
+    &__prev {
+      justify-content: start;
+      &::after {
+        transform: rotate(180deg);
+      }
+    }
+    &__next {
+      justify-content: end;
+    }
+  }
+}
+</style>
---
+++ src/components/SCarouselItem.vue
@@ -0,0 +1,50 @@
+<template>
+  <div
+    class="s-carousel-item"
+    :class="{ 's-carousel-item_animated': isAnimated }"
+    :style="{
+      transform: `translateX(calc(${(carouselItemData.translate * 100)}%))`,
+    }"
+  >
+    <img
+      class="s-carousel-item__image"
+      :src="getImageUrl(carouselItemData.image)"
+      alt="Product photo"
+    />
+  </div>
+</template>
+<script>
+export default {
+  name: "SCarouselItem",
+  props: {
+    carouselItemData: {
+      type: Object,
+      default: () => {},
+    },
+    isAnimated: {
+      type: Boolean,
+      default: () => {},
+    }
+  },
+  setup() {
+    const getImageUrl = (name) => {
+      return new URL(`../assets/images/publication/${name}`, import.meta.url)
+        .href;
+    };
+    return { getImageUrl };
+  },
+};
+</script>
+<style lang="scss">
+.s-carousel-item {
+  width: 1050px;
+  height: 100%;
+  &__image {
+    width: 100%;
+    height: 100%;
+  }
+}
+.s-carousel-item_animated {
+  transition: transform 1s ease;
+}
+</style>
