315af179418e915a85b8cc177f63dd6ae844a6ef
Kashirskiy Nikita <lordwebkit@gmail.com>
2022-12-18 14:25:54 +0300

    Created advantages section in main component

---
+++ src/components/SMain/SAdvantages.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="s-advantages-wrapper">
+    <div class="s-advantages">
+      <h2 class="s-title s-advantages__title">Our advantages</h2>
+      <div class="s-advantages__items">
+        <s-advantages-item
+          v-for="itemData in advantagesItemData"
+          :key="itemData"
+          :item-data="itemData"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import SAdvantagesItem from "./SAdvantagesItem.vue";
+
+export default {
+  components: {
+    SAdvantagesItem,
+  },
+  data() {
+    return {
+      advantagesItemData: [
+        {
+          title: "Handmade",
+          text: "All works presented are handmade",
+          classModification: "handmade"
+        },
+        {
+          title: "Personal pattern",
+          text: "All patterns were personally designed",
+          classModification: "pattern"
+        },
+        {
+          title: "Natural materials",
+          text: "All works are made from natural materials",
+          classModification: "material"
+        },
+        {
+          title: "Fast shipping",
+          text: " We cooperate with many companies",
+          classModification: "shipping"
+        },
+      ],
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.s-advantages {
+  @include grid();
+  &-wrapper {
+    padding: 40px 0;
+    margin-top: 50px;
+    background-color: #f8f8f8;
+  }
+  &__title {
+    @include title(32px);
+    text-align: center;
+    margin-bottom: 35px;
+  }
+  &__items {
+    display: flex;
+    justify-content: space-between;
+  }
+}
+</style>
---
+++ src/components/SMain/SAdvantagesItem.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="s-advantages-item" :class="`s-advantages-item_${itemData.classModification}`">
+    <div class="s-advantages-item__info">
+      <strong class="s-advantages-item__title">{{ itemData.title }}</strong>
+      <p class="s-advantages-item__text">{{ itemData.text }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "SAdvantagesItem",
+  props: {
+    itemData: {
+      type: Object,
+      default: () => {},
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.s-advantages-item {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  &::before {
+    content: "";
+    height: 50px;
+    width: 50px;
+    display: inline-block;
+  }
+  &__title {
+    @include title(18px);
+  }
+  &__info {
+    width: 70%;
+    margin: 0 auto;
+  }
+  &__text {
+    @include text(16px);
+  }
+  &_handmade {
+    &::before {
+        background: url("@/assets/icons/sewing-machine.svg");
+    }
+  }
+  &_pattern {
+    &::before {
+        background: url("@/assets/icons/pattern.svg");
+    }
+  }
+  &_material {
+    &::before {
+        background: url("@/assets/icons/material.svg");
+        padding: 5px;
+        height: 40px;
+        width: 40px;
+    }
+  }
+  &_shipping {
+    &::before {
+        background: url("@/assets/icons/shipping.svg");
+    }
+  }
+}
+</style>
