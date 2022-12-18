1df40e1b99a177b1974b849347a3f729b4dac634
Kashirskiy Nikita <lordwebkit@gmail.com>
2022-12-18 14:23:24 +0300

    Created popular section in main component

---
+++ src/components/SMain/SPopular.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="s-grid-wrapper s-popular">
+    <h2 class="s-title s-popular__title">Popular</h2>
+    <div class="s-popular__catalog">
+      <s-popular-item
+        v-for="popularItem in popular"
+        :key="popularItem"
+        class="s-popular__item"
+        :item-data="popularItem"
+      />
+    </div>
+  </div>
+</template>
+<script>
+import { mapActions, mapGetters } from "vuex";
+import SPopularItem from "./SPopularItem.vue";
+
+export default {
+  name: "SCollection",
+  components: {
+    SPopularItem,
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters("popular", ["popular"]),
+  },
+  mounted() {
+    this.getPopularFromApi();
+  },
+  methods: {
+    ...mapActions("popular", ["getPopularFromApi"]),
+  },
+};
+</script>
+<style lang="scss" scoped>
+.s-popular {
+  padding-top: 40px;
+  &__title {
+    text-align: center;
+    font-size: 48px;
+  }
+  &__catalog {
+    margin-top: 50px;
+  }
+  &__catalog {
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/SMain/SPopularItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="s-popular-item">
+    <router-link :to="{ name: 'catalog' }">
+      <img
+        class="s-popular-item__image"
+        :src="getImageUrl(itemData.image, 'popular')"
+        alt="Photo popular t-shirt"
+      />
+    </router-link>
+  </div>
+</template>
+<script>
+export default {
+  name: "SPopularItem",
+  props: {
+    itemData: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  setup() {
+    const getImageUrl = (name, imgDir) => {
+      return new URL(`../../assets/images/${imgDir}/${name}`, import.meta.url)
+        .href;
+    };
+    return { getImageUrl };
+  },
+};
+</script>
+<style lang="scss">
+.s-popular-item {
+  &:nth-child(3),
+  &:nth-child(4) {
+    margin-top: 60px;
+  }
+  &__image {
+    height: 100%;
+    display: block;
+  }
+}
+</style>
